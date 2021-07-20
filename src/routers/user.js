36fec1e57dbf061ed21b6d7fbcf84a872db098d2
Thapelo Mokoena <thapelomokoena@Thapelos-MBP.lan>
2021-07-20 11:43:35 +0200

    adding all

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", async (req, res) => {
-  res.send("Hello World! This is Oplay");
+  res.send("Hello World! This is Oplay to play");
 });
 
 module.exports = router;
